AquariOS/frameworks_base

BatteryBar: Fix vertical gradient
Change-Id: I72de7670d15a75c1f75a3858255ec8e2d8c878f8

packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBar.java
@@ -152,12 +152,16 @@ public class BatteryBar extends RelativeLayout implements Animatable {
             // charger
             mChargerLayout = new LinearLayout(mContext);
 
-            if (vertical)
+            if (vertical) {
+				if (mBarGradient != null) {
+					mBarGradient.setOrientation(GradientDrawable.Orientation.BOTTOM_TOP);
+				}
                 addView(mChargerLayout, new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                         pixels));
-            else
+            } else {
                 addView(mChargerLayout, new RelativeLayout.LayoutParams(pixels,
                         LayoutParams.MATCH_PARENT));
+            }
 
             mCharger = new View(mContext);
             mChargerLayout.setVisibility(View.GONE);

duRAN-test/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBar.java
@@ -152,12 +152,16 @@ public class BatteryBar extends RelativeLayout implements Animatable {
             // charger
             mChargerLayout = new LinearLayout(mContext);
 
-            if (vertical)
+            if (vertical) {
+                if (mBarGradient != null) {
+                    mBarGradient.setOrientation(GradientDrawable.Orientation.TOP_BOTTOM);
+                }
                 addView(mChargerLayout, new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                         pixels));
-            else
+            } else {
                 addView(mChargerLayout, new RelativeLayout.LayoutParams(pixels,
                         LayoutParams.MATCH_PARENT));
+            }
 
             mCharger = new View(mContext);
             mChargerLayout.setVisibility(View.GONE);